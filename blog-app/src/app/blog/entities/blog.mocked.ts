cfb58efdd00c693a0ca70984f5f3912456199b18
John Harrison <john@johnharrison.me>
2016-07-23 16:10:08 +0100

    adding mocked blogs

---
+++ blog-app/src/app/blog/entities/blog.mocked.ts
@@ -0,0 +1,6 @@
+import { Blog } from './blog'
+
+export const BLOGS: Blog[] = [
+    { id: 11, title: 'Some Crap', body: 'Hello yes this is blog.'},
+    { id: 12, title: 'More Crap', body: 'Hello yes this is another blog.'}
+]
