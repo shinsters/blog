74b4ab771ae3d17b0e5b44a11e91735531237c11
John Harrison <john@johnharrison.me>
2016-07-23 18:22:00 +0100

    bootstrap

--- blog-app/src/app/blog/card/blog-card.component.ts
+++ blog-app/src/app/blog/card/blog-card.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
 
 @Component({
   moduleId: module.id,
   selector: 'app-blog-card',
   templateUrl: 'blog-card.component.html',
-  styleUrls: ['blog-card.component.css']
+  styleUrls: ['blog-card.component.css'],
+  directives: [MD_CARD_DIRECTIVES]
 })
 export class BlogCardComponent implements OnInit {
 
