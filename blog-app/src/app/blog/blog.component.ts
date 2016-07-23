77b0dbad2420fcbcbd10aa429f04eaf4437d7f1d
John Harrison <john@johnharrison.me>
2016-07-23 16:37:02 +0100

    blog repository working and injecting

--- blog-app/src/app/blog/blog.component.ts
+++ blog-app/src/app/blog/blog.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { BlogRepository } from './services/blog.repository'
+
 @Component({
   moduleId: module.id,
   selector: 'app-blog',
   templateUrl: 'blog.component.html',
-  styleUrls: ['blog.component.css']
+  styleUrls: ['blog.component.css'],
+  providers: [BlogRepository]
 })
 export class BlogComponent implements OnInit {
 
-  constructor() { }
+  constructor(private blogRepository: BlogRepository) { }
 
   ngOnInit() {
   }
